80b54606616f26d72dd3a5f4cf56603eda7c2f4c
Sharienn-e <jacquelinnesnowtalon@gmail.com>
2025-07-09 19:18:06 +0200

    FEAT: simple page template

---
+++ script.js
@@ -0,0 +1,8 @@
+// Simple example: Smooth scrolling for navigation links
+document.querySelectorAll("nav a").forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute("href"));
+    target?.scrollIntoView({ behavior: "smooth" });
+  });
+});
